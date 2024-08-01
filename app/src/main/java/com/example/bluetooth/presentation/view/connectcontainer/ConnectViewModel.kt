055a3e64e15692b031047da9d69237784690e87f
Jitbatik <kvazzsik@gmail.com>
2024-08-01 15:11:47 +0300

    прослушка за статусом Bluetooth(рабочее)

--- app/src/main/java/com/example/bluetooth/presentation/view/connectcontainer/ConnectViewModel.kt
+++ app/src/main/java/com/example/bluetooth/presentation/view/connectcontainer/ConnectViewModel.kt
@@ -1,11 +1,11 @@
-package com.example.bluetooth
+package com.example.bluetooth.presentation.view.connectcontainer
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.domain.model.BluetoothDevice
-import com.example.domain.repository.BluetoothRepository
 import com.example.domain.repository.ConnectRepository
+import com.example.domain.repository.ScannerRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
@@ -19,18 +19,15 @@ private const val CONNECT_VIEWMODEL = "CONNECT_VIEWMODEL"
 
 @HiltViewModel
 class ConnectViewModel @Inject constructor(
-    private val bluetoothRepository: BluetoothRepository,
-    private val connectRepository: ConnectRepository
+    private val scannerRepository: ScannerRepository,
+    private val connectRepository: ConnectRepository,
 ) : ViewModel() {
-
-    private val _isBluetoothEnabled = MutableStateFlow(true)
+    private val _isBluetoothEnabled = scannerRepository.isBluetoothActive.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.Eagerly,
+        initialValue = false
+    )
     val isBluetoothEnabled: StateFlow<Boolean> = _isBluetoothEnabled
-
-//    val isBTActive = bluetoothRepository.isBluetoothActive.stateIn(
-//        scope = viewModelScope,
-//        started = SharingStarted.Eagerly,
-//        initialValue = false
-//    )
 
     private val _devices = MutableStateFlow<List<BluetoothDevice>>(emptyList())
     val devices: StateFlow<List<BluetoothDevice>>
@@ -43,7 +40,7 @@ class ConnectViewModel @Inject constructor(
     private fun observeBluetoothDeviceList() {
         Log.d(CONNECT_VIEWMODEL, "Subscribe to a stream")
         viewModelScope.launch {
-            bluetoothRepository.bluetoothDeviceList.collect { newDevices ->
+            scannerRepository.bluetoothDeviceList.collect { newDevices ->
                 _devices.value = newDevices
             }
         }
@@ -63,7 +60,7 @@ class ConnectViewModel @Inject constructor(
     fun findToDevice() {
         Log.d(CONNECT_VIEWMODEL, "Scanning for devices")
         viewModelScope.launch {
-            val result = bluetoothRepository.findPairedDevices()
+            val result = scannerRepository.findPairedDevices()
 
             result.onFailure { exception ->
                 Log.e("ConnectViewModel", "Failed to find devices", exception)
