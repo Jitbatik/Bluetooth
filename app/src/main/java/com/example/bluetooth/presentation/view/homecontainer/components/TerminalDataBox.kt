dec8252f44a638e4ef6694c88845e72e01078079
PSIS Vladimir <vvastapokvpol@gmail.com>
2024-08-16 17:32:56 +0300

    экран с данными изменяемый и в цвет неподключенный шрифт

--- app/src/main/java/com/example/bluetooth/presentation/view/homecontainer/components/TerminalDataBox.kt
+++ app/src/main/java/com/example/bluetooth/presentation/view/homecontainer/components/TerminalDataBox.kt
@@ -2,69 +2,90 @@ package com.example.bluetooth.presentation.view.homecontainer.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.bluetooth.R
 import com.example.bluetooth.ui.theme.BluetoothTheme
-
-val psisFontFamily = FontFamily(
-    Font(R.font.psis, FontWeight.Black)
-)
 
 
 @Composable
-fun TerminalDataBox() {
-    val sentence = "This is an example sentence for displaying characters in a grid layout."
-    val chars =  sentence.take(80).toList()
+fun TerminalDataBox(charsWithStyles: List<Pair<Char, Pair<Color, Color>>>, rows: Int) {
+    val charsPerRow = (charsWithStyles.size + rows - 1) / rows
 
     Box(
         modifier = Modifier
             .background(Color(0xFF61D7A4))
             .padding(4.dp)
+            .fillMaxWidth()
     ) {
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(20),
-            modifier = Modifier.padding(0.dp)
-        ) {
-            items(chars) { char ->
-                Box(
-                    modifier = Modifier
-                        .padding(0.dp)
-                        .background(Color.Transparent)
-                ) {
-                    Text(
-                        text = char.toString(),
-                        style = TextStyle(
-                            //fontFamily = psisFontFamily,
-                            fontSize = 16.sp,
-                            lineHeight = 16.sp
-                        ),
-                        modifier = Modifier.padding(0.dp)
-                    )
+        Column(modifier = Modifier.fillMaxWidth()) {
+            charsWithStyles.chunked(charsPerRow).take(rows).forEach { row ->
+                val rowText = buildAnnotatedString {
+                    row.forEach { (char, colors) ->
+                        val (textColor, backgroundColor) = colors
+                        withStyle(
+                            SpanStyle(
+                                color = textColor,
+                                background = backgroundColor,
+                            )
+                        ) {
+                            append(char)
+                        }
+                    }
                 }
+
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight(),
+                    text = rowText,
+                    style = TextStyle(
+                        fontFamily = FontFamily.Monospace,
+                        fontSize = 25.sp,
+                        lineHeight = 20.sp,
+                        letterSpacing = 0.sp
+                    ),
+                    textAlign = TextAlign.Center,
+                )
             }
         }
     }
 }
 
+
 @PreviewLightDark
 @Composable
 private fun TerminalDataBoxPreview() = BluetoothTheme {
     Surface {
-        TerminalDataBox()
+        val sentence =
+            "Процессор: СР6786   v105  R2  17.10.2023СКБ ПСИС www.psis.ruПроцессор остановлен"
+
+        fun getRandomColor(): Color {
+            val r = (0..255).random()
+            val g = (0..255).random()
+            val b = (0..255).random()
+            return Color(r, g, b)
+        }
+
+        val data = sentence.map { char ->
+            Pair(char, Pair(getRandomColor(), getRandomColor()))
+            //Pair(char, Pair(Color.Black, Color.Gray))
+        }
+        TerminalDataBox(data, 4)
     }
 }
