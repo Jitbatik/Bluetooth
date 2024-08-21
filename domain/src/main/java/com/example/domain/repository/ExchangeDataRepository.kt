fdc0d38bb99c1e6a3ea435c9f2d88a86503a42c8
PSIS Vladimir <vvastapokvpol@gmail.com>
2024-08-21 13:19:57 +0300

    прослушка за данными во вьюмодели

--- domain/src/main/java/com/example/domain/repository/ExchangeDataRepository.kt
+++ domain/src/main/java/com/example/domain/repository/ExchangeDataRepository.kt
@@ -4,7 +4,7 @@ package com.example.domain.repository
 import kotlinx.coroutines.flow.Flow
 
 interface ExchangeDataRepository {
-    val isSocket: Flow<Boolean>
-//    fun readFromStream(canRead: Boolean): Flow<ByteArray>
-//    suspend fun sendToStream(value: String): Result<Boolean>
+    fun getStateSocket(): Flow<Boolean>
+    fun readFromStream(canRead: Boolean): Flow<ByteArray>
+    //suspend fun sendToStream(value: String): Result<Boolean>
 }
