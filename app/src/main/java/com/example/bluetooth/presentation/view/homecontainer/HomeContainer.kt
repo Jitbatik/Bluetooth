ef877bf3e13c478d53965322cc0c18be01f61d61
PSIS Vladimir <vvastapokvpol@gmail.com>
2024-08-22 15:15:43 +0300

    исправлена отправка в поток рабочее чтение и отправка

--- app/src/main/java/com/example/bluetooth/presentation/view/homecontainer/HomeContainer.kt
+++ app/src/main/java/com/example/bluetooth/presentation/view/homecontainer/HomeContainer.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -44,6 +46,9 @@ fun HomeContainer(
             SpacerDivider()
             ButtonFBox(buttonType = false, onButtonClick = {})
             ButtonHelpBox(onButtonClick = {})
+            Button(onClick = { viewModel.requestPacketData() }) {
+                Text(text = "test")
+            }
         }
     }
 }
