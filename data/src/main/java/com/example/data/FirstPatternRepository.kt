fae6341a81ed182bc4fbb80f85207203c22c97cc
Jitbatik <kvazzsik@gmail.com>
2024-09-02 12:09:27 +0300

    для теста код

--- data/src/main/java/com/example/data/FirstPatternRepository.kt
+++ data/src/main/java/com/example/data/FirstPatternRepository.kt
@@ -6,7 +6,6 @@ import com.example.data.bluetooth.provider.BluetoothSocketProvider
 import com.example.data.bluetooth.utils.mapToListCharDataFromArray
 import com.example.domain.model.CharData
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.withContext
@@ -26,95 +25,151 @@ class FirstPatternRepository @Inject constructor(
 //            .distinctUntilChanged()
 //    }
 
+
     suspend fun requestData(): Flow<List<CharData>> = flow {
         var currentIndex = 0
         val listByteArray = mutableListOf<ByteArray>()
 
         while (currentIndex < 20) {
-            val command = listOf(
-                0xFE,
-                0x08,
+            val command = byteArrayOf(
+                0xFE.toByte(),
+                0x08.toByte(),
                 0x00,
                 0x00,
                 0x00,
-                currentIndex,
+                currentIndex.toByte(),
                 0x00,
                 0x00,
                 0x00,
                 0x00
             )
-            val byteArray = command.map { it.toByte() }.toByteArray()
 
             var request: ByteArray? = null
-            val timeoutDuration = 3000L
+
             while (request == null) {
-                sendToStream(byteArray)
-                request = withTimeoutOrNull(timeoutDuration) {
-                    readFromStream()
-                }
                 if (request == null) {
-                    delay(1000)
+                    Log.d(FIRST_PATTERN_REPOSITORY, "tttttt")
+                }
+                sendToStream(command)
+                request = withTimeoutOrNull(4000) {
+                    withContext(Dispatchers.IO) {
+                        readFromStream()
+                    }
+                }
+                if (request != null) {
+                    listByteArray.add(request)
                 }
             }
-
-            listByteArray.add(request)
-            currentIndex = (currentIndex + 1)
+            currentIndex++
         }
+
         emit(listByteArray.mapToListCharDataFromArray())
     }
+
 
     private fun getSocket(): BluetoothSocket? {
         return bluetoothSocketProvider.bluetoothSocket.value
     }
 
+
     private fun readFromStream(): ByteArray? {
         val socket = getSocket()
-        if (socket == null) {
-            Log.d(FIRST_PATTERN_REPOSITORY, "Socket is null")
-            return null
-        }
-        if (!socket.isConnected) {
-            Log.d(FIRST_PATTERN_REPOSITORY, "Socket is not connected")
-            return null
-        }
-        val inputStream = socket.inputStream ?: return null
 
-        val buffer = ByteArray(1024)
+        if (socket == null || !socket.isConnected) {
+            Log.d(
+                FIRST_PATTERN_REPOSITORY,
+                "Socket is ${if (socket == null) "null" else "not connected"}"
+            )
+            return null
+        }
+
         return try {
-            Log.d(FIRST_PATTERN_REPOSITORY, "Attempting to read from stream...")
+            val inputStream = socket.inputStream ?: return null
+            val buffer = ByteArray(1024)
             val bytesRead = inputStream.read(buffer)
-            val result = buffer.copyOf(bytesRead)
-            Log.d(FIRST_PATTERN_REPOSITORY, "Read from stream: [${result.joinToString(" ")}]")
-            result
+            if (bytesRead != -1) {
+                buffer.copyOf(bytesRead).also {
+                    Log.d(FIRST_PATTERN_REPOSITORY, "Read from stream: [${it.joinToString(" ")}]")
+                }
+            } else {
+                null
+            }
+
+
         } catch (e: IOException) {
             Log.e(FIRST_PATTERN_REPOSITORY, "Error reading stream", e)
             null
         }
     }
 
+
+//    private fun readFromStream(): ByteArray? {
+//        val socket = getSocket()
+//
+//        if (socket == null || !socket.isConnected) {
+//            Log.d(
+//                FIRST_PATTERN_REPOSITORY,
+//                "Socket is ${if (socket == null) "null" else "not connected"}"
+//            )
+//            return null
+//        }
+//
+//        val executor = Executors.newSingleThreadExecutor()
+//        val future: Future<ByteArray?> = executor.submit(Callable {
+//            try {
+//                val inputStream = socket.inputStream ?: return@Callable null
+//                val buffer = ByteArray(1024)
+//                val bytesRead = inputStream.read(buffer)
+//                return@Callable if (bytesRead != -1) {
+//                    buffer.copyOf(bytesRead).also {
+//                        Log.d(
+//                            FIRST_PATTERN_REPOSITORY,
+//                            "Read from stream: [${it.joinToString(" ")}]"
+//                        )
+//                    }
+//                } else {
+//                    null
+//                }
+//            } catch (e: IOException) {
+//                Log.e(FIRST_PATTERN_REPOSITORY, "Error reading stream", e)
+//                null
+//            }
+//        })
+//
+//        return try {
+//            // Тайм-аут 4 секунды
+//            future.get(4, TimeUnit.SECONDS)
+//
+//        } catch (e: TimeoutException) {
+//            Log.e(FIRST_PATTERN_REPOSITORY, "Read timed out after 4 seconds")
+//            null
+//        } catch (e: Exception) {
+//            Log.e(FIRST_PATTERN_REPOSITORY, "Error reading stream", e)
+//            null
+//        } finally {
+//            future.cancel(true)
+//            executor.shutdown() // Прекращаем работу executor
+//        }
+//    }
+
+
     private suspend fun sendToStream(value: ByteArray): Result<Boolean> {
-
         return withContext(Dispatchers.IO) {
-            try {
-                val socket = getSocket()
-                if (socket == null) {
-                    Log.d(FIRST_PATTERN_REPOSITORY, "Socket is null")
-                    return@withContext Result.failure(SecurityException("No connected socket"))
-                }
-                if (!socket.isConnected) {
-                    return@withContext Result.failure(SecurityException("Socket is not connected"))
-                }
+            val socket = getSocket()
 
-                val outputStream = socket.outputStream
-                    ?: return@withContext Result.failure(IOException("Output stream is null"))
+            if (socket == null || !socket.isConnected) {
+                val error = "Socket is ${if (socket == null) "null" else "not connected"}"
+                Log.d(FIRST_PATTERN_REPOSITORY, error)
+                return@withContext Result.failure(SecurityException(error))
+            }
 
+            return@withContext try {
+                val outputStream = socket.outputStream ?: return@withContext Result.failure(
+                    IOException("Output stream is null")
+                )
                 outputStream.write(value)
                 outputStream.flush()
-
-                Log.d(
-                    FIRST_PATTERN_REPOSITORY,
-                    "WRITTEN TO STREAM: ${value.joinToString(" ")}"
-                )
+                Log.d(FIRST_PATTERN_REPOSITORY, "Written to stream: ${value.joinToString(" ")}")
                 Result.success(true)
             } catch (e: IOException) {
                 Log.e(FIRST_PATTERN_REPOSITORY, "Error sending data to stream", e)
