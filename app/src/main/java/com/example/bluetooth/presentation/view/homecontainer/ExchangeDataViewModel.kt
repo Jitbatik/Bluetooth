2fb4fb3d7f813ac9859249f717f10fe9772c8903
PSIS Vladimir <vvastapokvpol@gmail.com>
2024-09-10 16:07:05 +0300

    рабочий обмен данными/не понятная отписка флоу

--- app/src/main/java/com/example/bluetooth/presentation/view/homecontainer/ExchangeDataViewModel.kt
+++ app/src/main/java/com/example/bluetooth/presentation/view/homecontainer/ExchangeDataViewModel.kt
@@ -24,7 +24,10 @@ import com.example.bluetooth.utils.UIEvents.RequestData
 import com.example.domain.repository.ExchangeDataRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -36,6 +39,22 @@ class ExchangeDataViewModel @Inject constructor(
 ) : ViewModel() {
     private val _data = MutableStateFlow<List<CharUI>>(emptyList())
     val data: StateFlow<List<CharUI>> = _data
+//    val data: StateFlow<List<CharUI>> = exchangeDataRepository.getData()
+//        .map { charDataList ->
+//            charDataList.map { charData ->
+//                CharUI(
+//                    char = charData.charByte.toInt().toChar(),
+//                    color = Color.Black,
+//                    background = Color.Transparent
+//                )
+//            }
+//        }
+//        .stateIn(
+//            scope = viewModelScope,
+//            started = SharingStarted.Eagerly,
+//            initialValue = emptyList()
+//        )
+
 
     private val _sentence =
         "Процессор: СР6786   v105  R2  17.10.2023СКБ ПСИС www.psis.ruПроцессор остановлен"
