f97f034add9ec670ba75df2bc8a6e1ca04a48dac
Jitbatik <kvazzsik@gmail.com>
2024-10-07 14:19:04 +0300

    поправки: текстовый провайдер как ресурс в коде

--- app/src/main/java/com/example/bluetooth/presentation/view/connect/components/permission/TextPermissionProvider.kt
+++ app/src/main/java/com/example/bluetooth/presentation/view/connect/components/permission/TextPermissionProvider.kt
@@ -1,10 +1,15 @@
 package com.example.bluetooth.presentation.view.connect.components.permission
 
+import androidx.annotation.StringRes
+import com.example.bluetooth.R
+
 class TextPermissionProvider : TextProvider {
-    override val title: String
-        get() = "Пожалуйста, предоставьте разрешения"
-    override val description: String
-        get() = "Приложению для сканирования и подключения требуются разрешения на эти действия. После обновите экран."
-    override val buttonText: String
-        get() = "Перейти в настройки"
+    @StringRes
+    override val title = R.string.text_provider_title_bluetooth
+
+    @StringRes
+    override val description = R.string.text_provider_description_bluetooth
+
+    @StringRes
+    override val buttonText = R.string.text_provider_button_bluetooth
 }
