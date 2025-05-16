3ca8a610147ee6add343f0781329148559f603c7
Honor PSIS <vrityvirt@gmail.com>
2025-05-16 14:16:17 +0300

    Crate state from SettingViewModel

---
+++ app/src/main/java/com/example/bluetooth/presentation/view/settings/model/SettingsState.kt
@@ -0,0 +1,9 @@
+package com.example.bluetooth.presentation.view.settings.model
+
+import com.example.bluetooth.model.ChartSettingsUI
+
+data class SettingsState(
+    val chartSettings: ChartSettingsUI,
+    val wirelessBluetoothMask: WirelessBluetoothMask,
+    val onEvents: (SettingsEvent) -> Unit,
+)
--- app/src/main/java/com/example/bluetooth/presentation/view/settings/model/WirelessBluetoothMask.kt
+++ app/src/main/java/com/example/bluetooth/presentation/view/settings/model/WirelessBluetoothMask.kt
@@ -1,6 +1,6 @@
 package com.example.bluetooth.presentation.view.settings.model
 
-data class WirelessNetworkState(
+data class WirelessBluetoothMask(
     val isEnabled: Boolean = false,
     val mask: String = ""
 )
