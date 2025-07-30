11c9f182da13ce509b6be1557eb7b3df2191076f
Honor PSIS <vrityvirt@gmail.com>
2025-07-30 10:39:14 +0300

    Add rssi for Bluetooth device from startDiscovery

--- app/src/main/java/com/example/bluetooth/presentation/view/connect/components/scanner/DeviceCard.kt
+++ app/src/main/java/com/example/bluetooth/presentation/view/connect/components/scanner/DeviceCard.kt
@@ -1,7 +1,9 @@
 package com.example.bluetooth.presentation.view.connect.components.scanner
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -12,6 +14,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -40,12 +43,24 @@ fun DeviceCard(
             .padding(8.dp)
             .clickable { onConnect(bluetoothDevice) }
     ) {
-        Column(
-            modifier = Modifier.padding(16.dp)
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Text(text = bluetoothDevice.name, fontWeight = FontWeight.Bold, color = textColor)
-            Spacer(modifier = Modifier.height(4.dp))
-            Text(text = bluetoothDevice.address, color = textColor)
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text(text = bluetoothDevice.name, fontWeight = FontWeight.Bold, color = textColor)
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(text = bluetoothDevice.address, color = textColor)
+            }
+            Text(
+                text = "${bluetoothDevice.rssi}",
+                fontWeight = FontWeight.Bold,
+                color = textColor,
+                modifier = Modifier.padding(end = 16.dp)
+            )
         }
     }
 }
