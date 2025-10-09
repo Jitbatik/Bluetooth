b2d0938f8a1519846febab276747c8cc38e32f20
PSIS Vladimir <vvastapokvpol@gmail.com>
2025-10-09 11:48:58 +0300

    chore(CentralControllerSetting): подготовил костяк для задачи

--- app/src/lift/kotlin/override/ui/Home.kt
+++ app/src/lift/kotlin/override/ui/Home.kt
@@ -1,17 +1,28 @@
 package override.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.NonRestartableComposable
 import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -59,9 +70,62 @@ fun Home(state: HomeState) {
                     .wrapContentSize()
                     .fillMaxWidth()
             )
+
+            // Todo Что нужно вообще сделать?
+
+            // Todo Парсер XMl - в нем храниться инфа для UI и адрессация по пакету, мин мах и
+            //  def (что это?)
+
+            // Todo Сверстать UI - уже основываясь на данных из XMl и функциональных кнопках
+
+            // Todo Подготовить DataExchangeViewModel - HomeState и onEvent
+
+            // Todo И уже после можно готовить часть связанную с Запросом - Парсингом Выводом
+
+            // Todo Конечный этап это Запись новых значений в Контроллер
+
+            // Todo По итогу можно будет проверять работу
+            CentralControllerSetting(
+                test = 0,
+                modifier = Modifier.fillMaxSize().padding(8.dp)
+            )
         }
     }
 }
+
+@Composable
+fun CentralControllerSetting(
+    test: Int,
+    modifier: Modifier = Modifier
+) {
+    when (test) {
+        1 -> ControllerListBox(modifier)
+        2 -> EmptyStateBox(modifier)
+        else -> NoConnectedBox(modifier)
+    }
+}
+
+@Composable
+fun ControllerListBox(modifier: Modifier = Modifier) {
+    // Todo Что тут должно быть
+    // Todo Список всех настроек с возможностью редактировать
+    // Todo Кнопки Обновить и Отправить
+
+    // Todo Возможна реализация где при прохождении N времени переход в EmptyStateBox
+}
+
+@Composable
+fun EmptyStateBox(modifier: Modifier = Modifier) {
+    // Todo Предупреждение о том что настройки еще не вычитанны
+    //  уже список из XML, но он не редактируемый
+    // Todo Переход в ControllerListBox при нажаитии кнопки Считать
+}
+
+@Composable
+fun NoConnectedBox(modifier: Modifier = Modifier) {
+    // Todo Предупреждение Нужно подключение к контроллеру для определения версии
+}
+
 
 object ButtonLists {
     val basic = listOf(
